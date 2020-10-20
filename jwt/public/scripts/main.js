71c652801314a2dbb49e71425b9ea0b2b86dbcfb
kenwschen <ken00535@gmail.com>
2020-10-20 23:06:53 +0800

    refactor(jwt): remove unused part

--- jwt/public/scripts/main.js
+++ jwt/public/scripts/main.js
@@ -1,26 +1,13 @@
 
-
 function login(user, password) {
-    // fetch('http://192.168.99.92:8080/login')
-    //     .then(function (response) {
-    //         console.log(response.status);
-    //         console.log(response.data);
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //         console.log(error.response);
-    //     })
-    console.log(user, password);
     axios.post('http://192.168.99.92:8080/login', {
         user: user,
         password: password,
+    }).then(function (response) {
+        console.log(response.status);
+        console.log(response.data);
+    }).catch(function (error) {
+        console.log(error);
+        console.log(error.response);
     })
-        .then(function (response) {
-            console.log(response.status);
-            console.log(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-            console.log(error.response);
-        })
 }
