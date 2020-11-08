d81b67bb7416b252c0635f0529021d25e12e4473
kenwschen <ken00535@gmail.com>
2020-11-08 23:33:28 +0800

    refactor(jwt): use template engine and css

--- jwt/index.js
+++ jwt/index.js
@@ -5,30 +5,57 @@ const jwt = require('jsonwebtoken')
 
 const SECRET = 'secret'
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({ extended: false }));
-app.post('/login', function (req, res) {
-    if (req.body.user === "user" && req.body.password === "pass") {
-        const token = jwt.sign({ user: req.body.user }, SECRET, { expiresIn: '1 day' })
-        res.status(201).send({ token })
-    } else {
-        res.status(401).send("Fail");
-    }
-})
-app.get('/content', function (req, res) {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', function (req, res) {
     const auth = req.header('Authorization')
     if (typeof auth === "undefined") {
-        res.status(401).send({ error: 'Please authenticate.' })
+        res.render('index', { username: "" });
         return
     }
     token = auth.replace('Bearer ', '')
     try {
         const decoded = jwt.verify(token, SECRET)
         if (decoded.user == "user") {
-            res.status(200).send({ location: '/content.html' })
+            res.render('index', {
+                username: decoded.user
+            });
+        }
+    } catch {
+        res.status(401).render('index', {});
+    }
+})
+
+app.post('/login', function (req, res) {
+    if (req.body.username === "user" && req.body.password === "pass") {
+        const token = jwt.sign({ user: req.body.username }, SECRET, { expiresIn: '1 day' })
+        res.json({
+            token
+        });
+    } else {
+        res.redirect('/');
+    }
+})
+
+app.get('/content', function (req, res) {
+    const auth = req.header('Authorization')
+    if (typeof auth === "undefined") {
+        res.status(401).send({ error: 'Please authenticate.' })
+        console.log(48)
+        return
+    }
+    token = auth.replace('Bearer ', '')
+    try {
+        const decoded = jwt.verify(token, SECRET)
+        if (decoded.user == "user") {
+            res.status(200).send({ data: 'Welcome!' })
         }
     } catch {
         res.status(401).send({ error: 'Please authenticate.' })
     }
 })
+
 app.listen(8080);
